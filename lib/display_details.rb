3fb9ca1e46026b5c397d3f3b9c407d865b7854a0
keitheous <keithchongwy@gmail.com>
2016-08-29 15:56:28 +1000

    OMG I got the display to work! OMG OMG

--- lib/display_details.rb
+++ lib/display_details.rb
@@ -2,7 +2,7 @@ require 'pry'
 require_relative 'sort_by_day'
 
 class Display
-  attr_accessor :a
+  attr_reader :a
 
   def initialize(bound = "nb")
     if bound == "nb"
@@ -15,18 +15,40 @@ class Display
   end
 
   def by_time_section(minutes = 60)
-    # puts minutes
-    if 60 % minutes != 0
-      puts "error" #learn how to raise error
-    else
-      puts @a
-      binding.pry
+    puts num_days = a[a.length][0]
+    daily_subject_hash = Hash.new
+    sections = 60 / minutes
+    frequency_table = {}
+
+    (1..num_days).each do |day|
+      puts "Day #{day}"
+      daily_subject_hash = a.select{|key, value| value[0] == day} #filter by day
+
+      (00..23).each do |hour|
+        low_bound = 0
+        high_bound = minutes - 1
+
+        sections.times do
+          (low_bound .. high_bound).each do |minute|
+            frequency_arr = []
+            @a.each do |key,value|
+              if value[1].hour == hour && value[1].min.between?(low_bound,high_bound)
+                frequency_arr << key
+              end
+            end
+          frequency_table["#{hour}:#{low_bound}"] = frequency_arr.count
+          end
+        low_bound += minutes
+        high_bound += minutes
+        end
+      end
+
     end
+
+  puts frequency_table
+
   end
 
 end
 
 Display.new.by_time_section
-# [3] pry(#<Display>)> @a.each do |key, value|
-# [3] pry(#<Display>)*   puts key or value
-# [3] pry(#<Display>)* end
