f1652df6c3b50d019b07a2b597609cb04bf2aa7f
keitheous <keithchongwy@gmail.com>
2016-08-30 15:51:44 +1000

    perfected display_details class hahaha

--- lib/display_details.rb
+++ lib/display_details.rb
@@ -2,53 +2,77 @@ require 'pry'
 require_relative 'sort_by_day'
 
 class Display
-  attr_reader :a
+  attr_reader :bound_subject, :num_days, :minutes, :sections
 
-  def initialize(bound = "nb")
+  def initialize(bound = "nb", min_user_input = 60)
+    if 60 % min_user_input != 0
+      raise 'error'
+    end
+
     if bound == "nb"
       bound = NorthBound.new.isolate_NB_vehicles_A
     elsif bound == "sb"
       bound = SorthBound.new.isolate_SB_vehicles_B
-   end
-    @a = Sorting.new.into_hashes(bound)
-    # binding.pry
-  end
-
-  def by_time_section(minutes = 60)
-    puts num_days = a[a.length][0]
-    daily_subject_hash = Hash.new
-    sections = 60 / minutes
-    frequency_table = {}
-
-    (1..num_days).each do |day|
-      puts "Day #{day}"
-      daily_subject_hash = a.select{|key, value| value[0] == day} #filter by day
-
-      (00..23).each do |hour|
-        low_bound = 0
-        high_bound = minutes - 1
-
-        sections.times do
-          (low_bound .. high_bound).each do |minute|
-            frequency_arr = []
-            @a.each do |key,value|
-              if value[1].hour == hour && value[1].min.between?(low_bound,high_bound)
-                frequency_arr << key
-              end
-            end
-          frequency_table["#{hour}:#{low_bound}"] = frequency_arr.count
-          end
-        low_bound += minutes
-        high_bound += minutes
-        end
-      end
-
     end
 
-  puts frequency_table
+    @bound_subject = Sorting.new.into_hashes(bound)
+    @num_days = bound_subject[bound_subject.length][0]
+    # @num_days can be adjustable if this was scalable - choose how many days to display
+    @minutes = min_user_input
+    @sections = 60 / min_user_input
 
   end
 
-end
+  def by_time_section
+    results_stored_daily = {} #the ultimate result of this class
+    # stored_hourly = {}
 
-Display.new.by_time_section
+    (1..num_days).each do |day|
+      puts "evaluating day #{day}"
+      # processing bound_subject one day at a time out of 5 days
+      daily_subject_hash = bound_subject.select{|daily_key, daily_value| daily_value[0] == day}
+
+      hourly_results = {}
+      # do hourly first
+      (00..23).each do |hour| #assess every hourly between 00 to 24 hour
+        puts "evaluating hour #{hour}"
+
+        sectionly_frequency = []
+        lower_limit = 0 #resets every hour, the clock starts at 00 minutes - lower sectioning
+        upper_limit = minutes - 1 #upper sectioning
+        stored_hourly = daily_subject_hash.select{|hourly_key, hourly_value| hourly_value[1].hour == hour}
+
+        (01..sections).each do |section| #split the evaluation into the sections
+
+          stored_hourly.each do |key, value| #now assess every minutely
+
+            if value[1].hour == hour && value[1].min.between?(lower_limit,upper_limit)
+              sectionly_frequency << "#{key} -> #{value[1].hour}:#{value[1].min} falls in  #{value[1].hour}:#{lower_limit}"
+            end #end of if stament
+
+          end #end of minutely iteration
+          #store the count and store individually
+          hourly_results["From #{hour}:#{lower_limit}"] = sectionly_frequency.count
+          lower_limit += minutes    #increase the limits based on sections
+          upper_limit += minutes
+
+
+        end #end of sections iteration
+
+      end #end for hourly iteration
+
+      results_stored_daily[day] = hourly_results #temporary memory
+
+    end #end for daily iteration
+
+    results_stored_daily #returned result in form of HASH of section counts in DAYS
+    binding.pry
+  end #end for method
+
+end #end of classs
+
+# Display.new("sb").by_time_section
+# Display.new("sb",15).by_time_section
+# Display.new("sb",30).by_time_section
+# Display.new("sb",6).by_time_section
+# Display.new("sb",7).by_time_section
