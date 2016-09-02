7eccb07f7b7949fc7fb27b3f5f0f435e4728592e
keitheous <keithchongwy@gmail.com>
2016-09-02 17:00:36 +1000

    doing test first

--- vehicle_counter.rb
+++ vehicle_counter.rb
@@ -1,13 +1,21 @@
 require './lib/displays'
+require './lib/times'
 
 def analyse_bound(bound)
-  bound == "nb" ? (puts "North Bound Analysis") : (puts "South Bound Analysis")
-  puts Displays.new(bound).sort_sections
+  bound == "nb" ? (puts "Analysing North Bound") : (puts "Analysing South Bound")
+  until
 
 end
 
 puts "Hello! Please pick a bound to be analysed."
-puts "NB - for North Bound"
-puts "SB - for South Bound"
-input = gets.chomp
-input.downcase == "nb" ? analyse_bound("nb") : analyse_bound("sb")
+puts "1. North Bound"
+puts "2. South Bound"
+choice_bound = gets.chomp
+
+if choice_bound.downcase == "north bound" || choice_bound == "1"
+  analyse_bound("nb")
+elsif choice_bound.downcase == "south bound" || choice_bound == "2"
+  analyse_bound("sb")
+else
+  puts "invalid choice - try again"
+end
